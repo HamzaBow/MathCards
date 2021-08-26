a7746a1dea1d0bb34c40d1e915f37c498ea5abb0
Hamza Bow <hamzabow77@gmail.com>
2021-08-26 19:07:16 +0100

    refactor: remove some unused imports, variables and some comments

--- src/components/main/SuccessSnackBar.js
+++ src/components/main/SuccessSnackBar.js
@@ -1,27 +1,12 @@
 import React from "react";
 import MuiAlert from "@material-ui/lab/Alert";
 import { Snackbar } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
 
 function Alert(props) {
   return <MuiAlert elevation={6} variant="filled" {...props} />;
 }
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    width: "100%",
-    "& > * + *": {
-      marginTop: theme.spacing(2),
-    },
-  },
-}));
-
 const SuccessSnackBar = ({ open, setActiveStep }) => {
-//   const [open, setOpen] = React.useState(false);
-
-//   const handleClick = () => {
-//     setOpen(true);
-//   };
   const handleClose = (event, reason) => {
     if (reason === "clickaway") {
       return;
