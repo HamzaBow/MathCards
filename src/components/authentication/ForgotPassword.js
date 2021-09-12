b6759192af199f6c61a0218e42226ba6a943c840
Hamza Bow <hamzabow77@gmail.com>
2021-09-12 15:37:55 +0100

    feat: change `ForgotPassword` page completely with new style + validation

--- src/components/authentication/ForgotPassword.js
+++ src/components/authentication/ForgotPassword.js
@@ -1,54 +1,198 @@
-import React, { useState } from 'react'
-import { useAuth } from '../../contexts/AuthContext'
-import { Redirect } from 'react-router'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import { Link as UiLink } from "@material-ui/core/";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import { Redirect, Link } from "react-router-dom";
 
+import { useAuth } from "../../contexts/AuthContext";
+import { Formik, Form, useField } from "formik";
+import { Alert } from "@material-ui/lab";
 
-const ForgotPassword = () => {
-  const [email, setEmail] = useState('')
-  const [error, setError] = useState('')
-  const [message, setMessage] = useState('')
-  const [loading, setLoading] = useState(false)
+import Logo from "../../logo/Logo";
 
-  const { resetPassword, currentUser } = useAuth()
+import * as yup from "yup";
 
-  async function handleSubmit(e) {
-    e.preventDefault()
-    try {
-      setError('')
-      setMessage('')
-      setLoading(true)
-      await resetPassword(email)
-      setMessage('Check your email for further instructions')
-      // history.push('/')
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    border: "2px solid lightgray",
+    borderRadius: "12px",
+    paddingTop: theme.spacing(3),
+    paddingLeft: theme.spacing(4),
+    paddingRight: theme.spacing(4),
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    // backgroundColor: theme.palette.secondary.main,
+    backgroundColor: "white",
+    color: "darkblue",
+    border: "2.5px solid darkblue",
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+  alert: {
+    width: "100%",
+    marginTop: theme.spacing(2),
+  },
+}));
 
-    } catch (err) {
-      setError('Failed to reset password')
-      console.error(err);
-    }
-    setLoading(false)
-  }
-
+function Copyright() {
   return (
-    <div style={{ paddingTop: "150px" }}>
-      { currentUser ?
-      <Redirect to='/' />
-      :
-      <>
-        {error && <h1 style={{ backgroundColor: 'red' }}>{error}</h1>}
-        {message && <h1 style={{ backgroundColor: 'lightgreen' }}>{message}</h1>}
-        {<h1 style={{ backgroundColor: 'green' }}>{currentUser && 'current user: ' + JSON.stringify(currentUser.email)}</h1>}
-        <form onSubmit={handleSubmit}>
-          <h1>Password Reset</h1>
-          <input type='email' value={email} onChange={(e) => setEmail(e.target.value)} placeholder='email' />
-          <button disabled={loading} type='submit' >Reset</button>
-          <h2><Link to='/login'>Login</Link></h2>
-        </form>
-      </>
-      }
-
-    </div>
-  )
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <UiLink color="inherit" href="https://www.mathcards.com/">
+        MathCards
+      </UiLink>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
 }
 
-export default ForgotPassword
+const CustomTextField = ({ type, label, autoFocus, ...props }) => {
+  const [field, meta] = useField(props);
+  const errorText = meta.error && meta.touched ? meta.error : "";
+  return (
+    <TextField
+      {...field}
+      type={type}
+      label={label}
+      autoFocus={autoFocus}
+      variant="outlined"
+      margin="normal"
+      fullWidth
+      helperText={errorText}
+      error={!!errorText}
+    />
+  );
+};
+
+const validationSchema = yup.object({
+  email: yup.string().email().required(),
+});
+
+export default function ForgotPassword() {
+  const classes = useStyles();
+
+  const [error, setError] = useState("");
+  const [message, setMessage] = useState("")
+
+  const { resetPassword, currentUser } = useAuth();
+  async function handleSubmit(data, { setSubmitting }) {
+    // e.preventDefault()
+    try {
+      setError("");
+      setMessage("");
+      setSubmitting(true);
+      await resetPassword(data.email);
+      setMessage('Check your email for further instructions')
+    } catch (err) {
+      setError("Failed to log in");
+      console.error(err);
+    }
+    setSubmitting(false);
+  }
+  return (
+    <>
+      {currentUser ? (
+        <Redirect to="/" />
+      ) : (
+        <Container component="main" maxWidth="sm">
+          <CssBaseline />
+          <div className={classes.paper}>
+            <Logo />
+            <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', gap: '0.5rem', marginTop: '1rem' }}>
+              <Avatar className={classes.avatar}>
+                <LockOutlinedIcon />
+              </Avatar>
+              <Typography component="h1" variant="h5" style={{marginRight: '2rem'}}>
+                Password Reset
+              </Typography>
+            </div>
+            {message && (
+              <div className={classes.alert}>
+                <Alert variant="filled" severity="success">
+                  {message}
+                </Alert>
+              </div>
+            )}
+            {error && (
+              <div className={classes.alert}>
+                <Alert variant="filled" severity="error">
+                  {error}
+                </Alert>
+              </div>
+            )}
+            <Formik
+              validateOnChange={true}
+              initialValues={{ email: "", password: "" }}
+              validationSchema={validationSchema}
+              onSubmit={handleSubmit}
+            >
+              {({
+                values,
+                isSubmitting,
+                handleChange,
+                handleBlur,
+                handleSubmit,
+              }) => (
+                <Form className={classes.form} noValidate>
+                  <CustomTextField
+                    name="email"
+                    type="email"
+                    label="Email Address"
+                  />
+                  <Button
+                    type="submit"
+                    disabled={isSubmitting}
+                    fullWidth
+                    variant="contained"
+                    color="primary"
+                    className={classes.submit}
+                  >
+                    Reset Password
+                  </Button>
+                  <Grid container>
+                    <Grid item xs>
+                      <Link to="/login">
+                        <UiLink href="/login" variant="body2">
+                          Login
+                        </UiLink>
+                      </Link>
+                    </Grid>
+                    <Grid item>
+                      <Link to="/signup">
+                        <UiLink href="/signup" variant="body2">
+                          {"Sign up"}
+                        </UiLink>
+                      </Link>
+                    </Grid>
+                  </Grid>
+                </Form>
+              )}
+            </Formik>
+          <Box mt={8}>
+            <Copyright />
+          </Box>
+          </div>
+        </Container>
+      )}
+    </>
+  );
+}
