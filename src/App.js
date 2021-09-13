caccefe8ec043d5d03884908723d10fe0078dbcf
Hamza Bow <hamzabow77@gmail.com>
2021-09-13 22:05:13 +0100

    style: make `app` theme same as `Operating system theme settings`
    if the user has chosen dark theme for his operating system,
    the App`s theme will also become dark

--- src/App.js
+++ src/App.js
@@ -27,6 +27,17 @@ import {
 import CssBaseline from "@material-ui/core/CssBaseline";
 
 function App() {
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          type: prefersDarkMode ? "dark" : "light",
+        },
+      }),
+    [prefersDarkMode]
+  );
   // ----------------------------- CARDS -----------------------------
 
   function cardsReducer(cards, action) {
