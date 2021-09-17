2f7d814b46142174973138cfb4d3011c07e78470
Hamza Bow <hamzabow77@gmail.com>
2021-09-17 23:03:28 +0100

    refactor: remove a ts error from `unused` footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,16 @@
-const Footer = () => {
-    return (
-        <footer>
-            <h1 style={footerStyle}>Footer</h1>            
-        </footer>
-    )
-}
-
-
 const footerStyle = {
     // textAlign: 'center',
     backgroundColor: "gray",
     paddingTop: "30px",
     paddingBottom: "30px",
+}
+
+const Footer = () => {
+    return (
+        <footer>
+            {/* <h1 style={footerStyle} >Footer</h1>             */}
+        </footer>
+    )
 }
 
 export default Footer
