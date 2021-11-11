90c66a808ae87a6a7a9927c5261e17b16afe2c43
Hamza Bow <hamzabow77@gmail.com>
2021-11-11 21:16:10 +0100

    refactor: create & use local `Props` & remove imported one

--- src/components/main/CardsList.tsx
+++ src/components/main/CardsList.tsx
@@ -1,7 +1,12 @@
 //  TODO: Card should be imported from ../cards/Card (finish implementing later).
+import { Action } from "App";
+import { CardInterface } from "components/cardform/CardForm";
 import Card from "../cards/Card";
-import { Props } from "./Main"
 
+interface Props {
+  cards: CardInterface[];
+  cardsDispatch: React.Dispatch<Action>;
+}
 
 const CardsList: React.FC<Props> = ({ cards, cardsDispatch }) => {
   // TODO: the container should have no visible style (except probably for margins, etc. ) to make the app style and layout SIMPLER
