2e74dc14dc83cd81a5b460ed3fab8e119b7a7782
Hamza Bow <hamzabow77@gmail.com>
2021-11-19 17:57:24 +0100

    refactor: format 'SaveToPrompt' file

--- src/components/cards/SaveToPrompt.tsx
+++ src/components/cards/SaveToPrompt.tsx
@@ -1,9 +1,16 @@
-import { ClickAwayListener, Grow, MenuItem, MenuList, Paper, Popper } from '@mui/material'
-import React from 'react'
+import {
+  ClickAwayListener,
+  Grow,
+  MenuItem,
+  MenuList,
+  Paper,
+  Popper,
+} from "@mui/material";
+import React from "react";
 import { ImSigma } from "react-icons/im";
-import { useUser } from '../../contexts/UserContext';
-import { Collection } from 'contexts/UserContext'
-import { fetchAddCardToCollection } from 'api/collectionAPI';
+import { useUser } from "../../contexts/UserContext";
+import { Collection } from "contexts/UserContext";
+import { fetchAddCardToCollection } from "api/collectionAPI";
 
 // ***********************************************************************************************************************
 // ***********************************************************************************************************************
@@ -15,18 +22,23 @@ interface CollectionItemProps {
   setSaveToPromptOpen: Function;
 }
 
-const CollectionItem: React.FC<CollectionItemProps> = ({ collectionId, cardId, title, setSaveToPromptOpen }) => {
+const CollectionItem: React.FC<CollectionItemProps> = ({
+  collectionId,
+  cardId,
+  title,
+  setSaveToPromptOpen,
+}) => {
   const handleSaveCardToCollection = (event: any) => {
-    fetchAddCardToCollection(collectionId, cardId )
-    setSaveToPromptOpen(false)
-  }
+    fetchAddCardToCollection(collectionId, cardId);
+    setSaveToPromptOpen(false);
+  };
   return (
-  <MenuItem onClick={handleSaveCardToCollection}>
-    <ImSigma style={{ marginRight: '0.7rem' }} />
+    <MenuItem onClick={handleSaveCardToCollection}>
+      <ImSigma style={{ marginRight: "0.7rem" }} />
       {title}
-  </MenuItem>
-  )
-}
+    </MenuItem>
+  );
+};
 
 // ***********************************************************************************************************************
 // ***********************************************************************************************************************
@@ -38,42 +50,61 @@ interface Props {
   saveRef: any;
 }
 
-const SaveToPrompt: React.FC<Props> = ({cardId, saveToPromptOpen, setSaveToPromptOpen, saveRef}) => {
-
+const SaveToPrompt: React.FC<Props> = ({
+  cardId,
+  saveToPromptOpen,
+  setSaveToPromptOpen,
+  saveRef,
+}) => {
   const user = useUser();
 
   // const handleClose = (event: React.MouseEvent<Document, MouseEvent>): void => {
   // const handleClose = (event: MouseEventHandler<HTMLLIElement>) => {
   const handleClose = (event: any) => {
-
     if (saveRef && saveRef.contains(event.target)) {
       return;
     }
     setSaveToPromptOpen(false);
-  }
+  };
 
   return (
-      <Popper open={saveToPromptOpen}  anchorEl={saveRef} role={undefined} placement="right" transition disablePortal>
-        {({ TransitionProps, placement }) => (
-          <Grow
-            {...TransitionProps}
-            style={{ transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom' }}
+    <Popper
+      open={saveToPromptOpen}
+      anchorEl={saveRef}
+      role={undefined}
+      placement="right"
+      transition
+      disablePortal
+    >
+      {({ TransitionProps, placement }) => (
+        <Grow
+          {...TransitionProps}
+          style={{
+            transformOrigin:
+              placement === "bottom" ? "center top" : "center bottom",
+          }}
           >
-            <Paper>
-              <ClickAwayListener onClickAway={handleClose}>
-                <MenuList autoFocusItem={saveToPromptOpen} id="menu-list-grow" > {/* onKeyDown={handleListKeyDown}> */}
-                  {user.collections.map((collection: Collection) => (
-                    <CollectionItem key={collection._id} collectionId={collection._id} cardId={cardId} title={collection.title} setSaveToPromptOpen={setSaveToPromptOpen}  />
-                  ))}
-                </MenuList>
+          <Paper>
+            <ClickAwayListener onClickAway={handleClose}>
+              <MenuList autoFocusItem={saveToPromptOpen} id="menu-list-grow">
+                {" "}
+                {/* onKeyDown={handleListKeyDown}> */}
+                {user.collections.map((collection: Collection) => (
+                  <CollectionItem
+                    key={collection._id}
+                    collectionId={collection._id}
+                    cardId={cardId}
+                    title={collection.title}
+                    setSaveToPromptOpen={setSaveToPromptOpen}
+                  />
+                ))}
+              </MenuList>
+            </ClickAwayListener>
+          </Paper>
+        </Grow>
+      )}
+    </Popper>
+  );
+};
 
-
-              </ClickAwayListener>
-            </Paper>
-          </Grow>
-        )}
-      </Popper>
-  )
-}
-
-export default SaveToPrompt
+export default SaveToPrompt;
