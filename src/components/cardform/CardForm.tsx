6d982862c773ade7c6a0f6702277511f2aa0b408
Hamza Bow <hamzabow77@gmail.com>
2021-11-21 23:24:43 +0100

    refactor: format 'CardForm' file

--- src/components/cardform/CardForm.tsx
+++ src/components/cardform/CardForm.tsx
@@ -1,11 +1,17 @@
-import React, { useState, useRef, useEffect, useReducer, Dispatch } from 'react'
-import { useParams } from 'react-router-dom'
-import FormFace from './FormFace'
-import FormOther from './formother/FormOther'
+import React, {
+  useState,
+  useRef,
+  useEffect,
+  useReducer,
+  Dispatch,
+} from "react";
+import { useParams } from "react-router-dom";
+import FormFace from "./FormFace";
+import FormOther from "./formother/FormOther";
 
-import CardFormStepper from './CardFormStepper'
+import CardFormStepper from "./CardFormStepper";
 import SuccessSnackBar from "./SuccessSnackBar";
-import Overlay from '../utilities/Overlay'
+import Overlay from "../utilities/Overlay";
 import { CARD_FORM_ACTIONS } from "../../Constants";
 import { Action } from "App";
 
@@ -23,7 +29,7 @@ export interface Field {
 
 export interface FrontNBackFields {
   front: Field[];
-  back: Field[]
+  back: Field[];
 }
 
 export interface DifficultyLevelsInterface {
@@ -38,152 +44,185 @@ export interface CardInterface {
   _id: string;
   front: Field[];
   back: Field[];
-  difficultyLevels: DifficultyLevelsInterface; 
+  difficultyLevels: DifficultyLevelsInterface;
   tags: string[];
 }
-export type OperationType = 'edit' | 'create';
+export type OperationType = "edit" | "create";
 interface Props {
-  operationType: OperationType; 
+  operationType: OperationType;
   cards?: CardInterface[];
-  cardsDispatch: Dispatch<Action> 
+  cardsDispatch: Dispatch<Action>;
 }
 
-const CardForm: React.FC<Props>  = ( { operationType, cards, cardsDispatch } ) => {
+const CardForm: React.FC<Props> = ({ operationType, cards, cardsDispatch }) => {
+  interface RouteParams {
+    id: string;
+  }
+  const params = useParams<RouteParams>();
 
-    interface RouteParams {
-      id: string;
+  // ******************************************* FIELDS *******************************************
+  function newField(id: number, fieldType: FieldType) {
+    if (fieldType === FieldType.Text) {
+      return {
+        id,
+        type: fieldType,
+        htmlContent: "",
+      };
     }
-    const params = useParams<RouteParams>();
 
-    // ******************************************* FIELDS *******************************************
-    function newField(id: number, fieldType: FieldType) {
-        if (fieldType === FieldType.Text) {
+    if (fieldType === FieldType.Math) {
+      return {
+        id,
+        type: fieldType,
+        latex: "",
+      };
+    }
+  }
+
+  function frontNBackFieldsReducer(
+    frontNBackFields: FrontNBackFields,
+    action: Action
+  ) {
+    type Face = "front" | "back";
+
+    let face: Face = "front";
+    let otherFace: Face = "back";
+
+    if (action.payload.face === "back") {
+      face = "back";
+      otherFace = "front";
+    }
+
+    switch (action.type) {
+      case CARD_FORM_ACTIONS.ADD_TEXT_QUILL:
         return {
-            id,
-            type: fieldType,
-            htmlContent: "",
+          [otherFace]: frontNBackFields[otherFace],
+          [face]: [
+            ...frontNBackFields[face],
+            newField(action.payload.id, FieldType.Text),
+          ],
         };
-        }
 
-        if (fieldType === FieldType.Math) {
+      case CARD_FORM_ACTIONS.ADD_MATH_QUILL:
         return {
-            id,
-            type: fieldType,
-            latex: "",
+          [otherFace]: frontNBackFields[otherFace],
+          [face]: [
+            ...frontNBackFields[face],
+            newField(action.payload.id, FieldType.Math),
+          ],
         };
-        }
+
+      case CARD_FORM_ACTIONS.UPDATE_LATEX:
+        return {
+          [otherFace]: [...frontNBackFields[otherFace]],
+          [face]: frontNBackFields[face].map((field) => {
+            if (field.id === action.payload.id) {
+              return { ...field, latex: action.payload.latex };
+            }
+            return field;
+          }),
+        };
+
+      case CARD_FORM_ACTIONS.UPDATE_HTML_CONTENT:
+        return {
+          [otherFace]: frontNBackFields[otherFace],
+          [face]: frontNBackFields[face].map((field) => {
+            if (field.id === action.payload.id) {
+              return { ...field, htmlContent: action.payload.htmlContent };
+            }
+            return field;
+          }),
+        };
+      case CARD_FORM_ACTIONS.SET_FIELDS:
+        return action.payload.frontNBackFields;
+
+      default:
+        return frontNBackFields;
     }
+  }
 
-    function frontNBackFieldsReducer(frontNBackFields: FrontNBackFields, action: Action) {
-        type Face = "front" | "back";
+  const [frontNBackFields, frontNBackFieldsDispatch] = useReducer(
+    frontNBackFieldsReducer,
+    { front: [], back: [], other: [] }
+  );
 
-        let face: Face = "front";
-        let otherFace: Face = "back";
+  // ****************************************** Difficulty Levels ****************************************
+  const [difficultyLevels, setDifficultyLevels] = useState({
+    veryEasy: false,
+    easy: false,
+    medium: false,
+    hard: false,
+    veryHard: false,
+  });
 
-        if(action.payload.face === 'back'){
-            face = 'back';
-            otherFace = 'front';
-        }
+  // ************************************************* Tags *********************************************
+  const [tags, setTags] = useState<string[]>([]);
 
-        switch (action.type) {
-            case CARD_FORM_ACTIONS.ADD_TEXT_QUILL:
-                return { [otherFace]: frontNBackFields[otherFace], [face]: [...frontNBackFields[face], newField(action.payload.id, FieldType.Text)]};
+  // *************************************** END OF NEW CARD-RELATED STATES ****************************************
 
-            case CARD_FORM_ACTIONS.ADD_MATH_QUILL:
-                return { [otherFace]: frontNBackFields[otherFace], [face]: [...frontNBackFields[face], newField(action.payload.id, FieldType.Math)]};
-
-            case CARD_FORM_ACTIONS.UPDATE_LATEX:
-                return {[otherFace]: [...frontNBackFields[otherFace]], [face]: frontNBackFields[face].map((field) => {
-                if(field.id === action.payload.id){
-                    return { ...field, latex: action.payload.latex };
-                }
-                return field;
-                })}
-
-            case CARD_FORM_ACTIONS.UPDATE_HTML_CONTENT:
-                return {[otherFace]: frontNBackFields[otherFace], [face]: frontNBackFields[face].map((field) => {
-                if(field.id === action.payload.id){
-                    return { ...field, htmlContent: action.payload.htmlContent };
-                }
-                return field;
-                })}
-            case CARD_FORM_ACTIONS.SET_FIELDS:
-                return action.payload.frontNBackFields;
-
-            default:
-                return frontNBackFields;
-        }
+  useEffect(() => {
+    if (operationType === "edit") {
+      const card: CardInterface = cards?.find(
+        (card: CardInterface) => card?._id === params.id
+      ) as CardInterface;
+      frontNBackFieldsDispatch({
+        type: CARD_FORM_ACTIONS.SET_FIELDS,
+        payload: { frontNBackFields: { front: card?.front, back: card?.back } },
+      });
+      setDifficultyLevels(card.difficultyLevels);
+      setTags(card.tags);
     }
+  }, []);
 
-    const [frontNBackFields, frontNBackFieldsDispatch] = useReducer(frontNBackFieldsReducer, {front: [], back: [], other: []});
+  //TODO: what if by mistake two properties are both true !!!, must figure out a better way to do this.
+  const [formState, setFormState] = useState({
+    front: true,
+    back: false,
+    other: false,
+  });
 
+  const [finished, setFinished] = useState(false);
 
+  const front = useRef<HTMLDivElement>(null);
+  const back = useRef<HTMLDivElement>(null);
+  const other = useRef<HTMLDivElement>(null); // other is the last form where the user adds tags and difficulty levels to the new card.
 
-    // ****************************************** Difficulty Levels ****************************************
-    const [difficultyLevels, setDifficultyLevels] = useState({veryEasy: false, easy: false, medium: false, hard: false, veryHard: false})
+  const [activeStep, setActiveStep] = React.useState(0);
 
-    // ************************************************* Tags *********************************************
-    const [tags, setTags] = useState<string[]>([])
+  useEffect(() => {
+    if (front !== null && front.current !== null) {
+      front.current.style.transform = "translate(  -50%, -150vh )";
+      front.current.style.animation = "transition: transform 0.2s ease-in-out";
+    } else {
+      console.error("front or front.current is null");
+    }
+  }, []);
 
+  useEffect(() => {
+    if (operationType === "create") {
+      document.title = "New Card";
+    }
+    if (operationType === "edit") {
+      document.title = "Edit Card";
+    }
+    return () => {
+      document.title = "MathCards";
+    };
+  }, [operationType]);
 
-    // *************************************** END OF NEW CARD-RELATED STATES ****************************************
-
-    useEffect(() => {
-        if(operationType === 'edit'){
-            const card: CardInterface = cards?.find((card: CardInterface) => card?._id === params.id) as CardInterface; 
-            frontNBackFieldsDispatch({type: CARD_FORM_ACTIONS.SET_FIELDS, payload: {frontNBackFields: {front: card?.front, back: card?.back} }})
-            setDifficultyLevels(card.difficultyLevels)
-            setTags(card.tags)
-        }
-    }, [])
-
-
-    //TODO: what if by mistake two properties are both true !!!, must figure out a better way to do this.
-    const [formState, setFormState] = useState({
-        front: true,
-        back: false,
-        other: false,
-    })
-
-    const [finished, setFinished] = useState(false);
-
-    const front = useRef<HTMLDivElement>(null)
-    const back = useRef<HTMLDivElement>(null)
-    const other = useRef<HTMLDivElement>(null)  // other is the last form where the user adds tags and difficulty levels to the new card.
-
-    const [activeStep, setActiveStep] = React.useState(0);
-
-    useEffect(() => {
-      if(front !== null && front.current !== null){
-        front.current.style.transform = "translate(  -50%, -150vh )";
-        front.current.style.animation = "transition: transform 0.2s ease-in-out";
-      } else {
-        console.error("front or front.current is null")
-      }
-    },[])
-
-    useEffect(() => {
-
-        if(operationType === 'create'){
-            document.title = 'New Card';
-        }
-        if(operationType === 'edit'){
-            document.title = 'Edit Card';
-        }
-        return () => {
-            document.title = 'MathCards';
-        }
-    },[operationType])
-
-    //TODO: FIXME: refactor the inside of this useEffect hook (probably requires refactoring the whole page)
-    useEffect(() => {
-
-      if(front !== null && front.current !== null &&
-         back !== null && back.current !== null &&
-         other !== null && other.current !== null){
+  //TODO: FIXME: refactor the inside of this useEffect hook (probably requires refactoring the whole page)
+  useEffect(() => {
+    if (
+      front !== null &&
+      front.current !== null &&
+      back !== null &&
+      back.current !== null &&
+      other !== null &&
+      other.current !== null
+    ) {
       if (formState.front === true) {
         front.current.style.transform = "translate(  -50%, -50% )";
-        
+
         back.current.style.transform = "translate( 100vw, -50% )";
 
         other.current.style.transform = "translate( 100vw, -50% )";
@@ -192,102 +231,100 @@ const CardForm: React.FC<Props>  = ( { operationType, cards, cardsDispatch } ) =
       if (formState.back === true) {
         front.current.style.transform = "translate(  -100vw, -50% )";
 
-        back.current.style.transform  = "translate(  -50%, -50% )";
+        back.current.style.transform = "translate(  -50%, -50% )";
 
         other.current.style.transform = "translate( 100vw, -50% )";
       }
 
-
       if (formState.other === true) {
         front.current.style.transform = "translate(  -100vw, -50% )";
 
-        back.current.style.transform  = "translate(  -100vw,  -50% )";
+        back.current.style.transform = "translate(  -100vw,  -50% )";
 
         other.current.style.transform = "translate(  -50%, -50% )";
       }
     } else {
-      console.error("`front`, `back` or `other` refs are null (or their current property is null)")
+      console.error(
+        "`front`, `back` or `other` refs are null (or their current property is null)"
+      );
     }
+  }, [formState]);
 
-    }, [formState]);
-    
-
-
-    //TODO: use useReducer instead of useState (dispatch instead of two functions)
-    function next(){
-        if(formState.front === true){
-            setFormState({front: false, back: true, other: false})            
-            setActiveStep((prevActiveStep) => prevActiveStep + 1);
-        }
-        if(formState.back === true){
-            setFormState({front: false, back: false, other: true})            
-            setActiveStep((prevActiveStep) => prevActiveStep + 1);
-        }
+  //TODO: use useReducer instead of useState (dispatch instead of two functions)
+  function next() {
+    if (formState.front === true) {
+      setFormState({ front: false, back: true, other: false });
+      setActiveStep((prevActiveStep) => prevActiveStep + 1);
     }
-
-    function prev(){
-        if(formState.back === true){
-            setFormState({front: true, back: false, other: false})            
-            setActiveStep((prevActiveStep) => prevActiveStep - 1);
-        }
-        if(formState.other === true){
-            setFormState({front: false, back: true, other: false})            
-            setActiveStep((prevActiveStep) => prevActiveStep - 1);
-        }
+    if (formState.back === true) {
+      setFormState({ front: false, back: false, other: true });
+      setActiveStep((prevActiveStep) => prevActiveStep + 1);
     }
+  }
 
-    return (
-      <>
-        <Overlay />
-        {!finished ? (
-          <>
-            {/* <CardFormHeader > {operationType === "create" ? "New Card" : "Edit Card"}</CardFormHeader> */}
-            <FormFace
-              ref={front}
-              face="front"
-              next={next}
-              frontNBackFields={frontNBackFields}
-              fieldsDispatch={frontNBackFieldsDispatch}
-            />
-            <FormFace
-              ref={back}
-              face="back"
-              next={next}
-              prev={prev}
-              frontNBackFields={frontNBackFields}
-              fieldsDispatch={frontNBackFieldsDispatch}
-            />
-            <FormOther
-              operationType={operationType}
-              ref={other}
-              prev={prev}
-              activeStep={activeStep}
-              setActiveStep={setActiveStep}
-              setFinished={setFinished}
-              difficultyLevels={difficultyLevels}
-              setDifficultyLevels={setDifficultyLevels}
-              tags={tags}
-              setTags={setTags}
-              frontNBackFields={frontNBackFields}
-              cardsDispatch={cardsDispatch}
-            />
-            <div
-              style={{
-                position: "fixed",
-                bottom: "0",
-                left: "25%",
-                right: "0",
-                width: "50%",
-              }}
-            >
-              <CardFormStepper activeStep={activeStep} />
-            </div>
-          </>
-        ) : (
-          <SuccessSnackBar />
-        )}
-      </>
-    );
-}
+  function prev() {
+    if (formState.back === true) {
+      setFormState({ front: true, back: false, other: false });
+      setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    }
+    if (formState.other === true) {
+      setFormState({ front: false, back: true, other: false });
+      setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    }
+  }
 
-export default CardForm
+  return (
+    <>
+      <Overlay />
+      {!finished ? (
+        <>
+          {/* <CardFormHeader > {operationType === "create" ? "New Card" : "Edit Card"}</CardFormHeader> */}
+          <FormFace
+            ref={front}
+            face="front"
+            next={next}
+            frontNBackFields={frontNBackFields}
+            fieldsDispatch={frontNBackFieldsDispatch}
+          />
+          <FormFace
+            ref={back}
+            face="back"
+            next={next}
+            prev={prev}
+            frontNBackFields={frontNBackFields}
+            fieldsDispatch={frontNBackFieldsDispatch}
+          />
+          <FormOther
+            operationType={operationType}
+            ref={other}
+            prev={prev}
+            activeStep={activeStep}
+            setActiveStep={setActiveStep}
+            setFinished={setFinished}
+            difficultyLevels={difficultyLevels}
+            setDifficultyLevels={setDifficultyLevels}
+            tags={tags}
+            setTags={setTags}
+            frontNBackFields={frontNBackFields}
+            cardsDispatch={cardsDispatch}
+          />
+          <div
+            style={{
+              position: "fixed",
+              bottom: "0",
+              left: "25%",
+              right: "0",
+              width: "50%",
+            }}
+          >
+            <CardFormStepper activeStep={activeStep} />
+          </div>
+        </>
+      ) : (
+        <SuccessSnackBar />
+      )}
+    </>
+  );
+};
+
+export default CardForm;
