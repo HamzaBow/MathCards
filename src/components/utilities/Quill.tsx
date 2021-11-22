a5cc6638eabc4217a96032676ec082f85ec9b97c
Hamza Bow <hamzabow77@gmail.com>
2021-11-22 15:05:05 +0100

    refactor: format Quill.tsx file

--- src/components/utilities/Quill.tsx
+++ src/components/utilities/Quill.tsx
@@ -1,35 +1,42 @@
-import ReactQuill from 'react-quill'
-import React from 'react'
-import 'react-quill/dist/quill.snow.css';
-import { CARD_FORM_ACTIONS } from '../../Constants';
+import ReactQuill from "react-quill";
+import React from "react";
+import "react-quill/dist/quill.snow.css";
+import { CARD_FORM_ACTIONS } from "../../Constants";
 
 interface Props {
   id: string | number;
   htmlContent: string;
   fieldsDispatch: Function;
-  face: 'front' | 'back';
+  face: "front" | "back";
 }
 
 const Quill: React.FC<Props> = ({ id, htmlContent, fieldsDispatch, face }) => {
+  const quillContainerStyle = {
+    width: "22rem",
+    // maxHeight: '5rem',
+    margin: "0.5rem",
+  };
 
-    const quillContainerStyle = {
-        width: '22rem',
-        // maxHeight: '5rem',
-        margin: '0.5rem',
-    }
+  const quillStyle: any = {
+    maxHeight: "15rem",
+    overflowY: "auto",
+  };
 
-    const quillStyle: any = {
-        maxHeight: '15rem',
-        overflowY: 'auto'
-    }
+  return (
+    <div style={quillContainerStyle}>
+      <ReactQuill
+        theme="snow"
+        style={quillStyle}
+        value={htmlContent}
+        onChange={(htmlText) => {
+          fieldsDispatch({
+            type: CARD_FORM_ACTIONS.UPDATE_HTML_CONTENT,
+            payload: { id: id, htmlContent: htmlText, face: face },
+          });
+        }}
+      />
+    </div>
+  );
+};
 
-    return (
-        <div style={quillContainerStyle} >
-            <ReactQuill theme="snow" style={quillStyle} value={htmlContent} onChange={(htmlText) => {
-                fieldsDispatch({ type: CARD_FORM_ACTIONS.UPDATE_HTML_CONTENT,
-                                 payload: {id: id, htmlContent: htmlText, face: face} })}} />
-        </div>
-    )
-}
-
-export default Quill
+export default Quill;
