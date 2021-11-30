037840bb9b4bcd0d89081e90be1998b27b5fa451
Hamza Bow <hamzabow77@gmail.com>
2021-11-30 06:37:28 +0100

    refactor: delete old commented code

--- src/components/cardform/CardFormStepper.tsx
+++ src/components/cardform/CardFormStepper.tsx
@@ -22,19 +22,6 @@ function getSteps() {
   return ['Front', 'Back', 'Other Info'];
 }
 
-// function getStepContent(stepIndex) {
-//   switch (stepIndex) {
-//     case 0:
-//       return 'Front';
-//     case 1:
-//       return 'Back';
-//     case 2:
-//       return 'Other Info';
-//     default:
-//       return 'Unknown stepIndex';
-//   }
-// }
-
 interface Props {
   activeStep: number;
 }
@@ -53,31 +40,7 @@ const CardFormStepper : React.FC<Props> = ({ activeStep }) => {
           </Step>
         ))}
       </Stepper>
-      {/* <div>
-        {activeStep === steps.length ? (
-          <div>
-            <Typography className={classes.instructions}>All steps completed</Typography>
-            <Button onClick={handleReset}>Reset</Button>
-          </div>
-        ) : (
-          <div>
-            <Typography className={classes.instructions}>{getStepContent(activeStep)}</Typography>
-            <div>
-              <Button
-                disabled={activeStep === 0}
-                onClick={handleBack}
-                className={classes.backButton}
-              >
-                Back
-              </Button>
-              <Button variant="contained" color="primary" onClick={handleNext}>
-                {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
-              </Button>
-            </div>
-          </div>
-        )}
-        </div> */}
-    </div> 
+    </div>
   );
 }
 
