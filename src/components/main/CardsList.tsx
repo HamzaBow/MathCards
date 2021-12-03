edf97da7d4aa95defa3e2e0a7706c5c566dde70e
Hamza Bow <hamzabow77@gmail.com>
2021-12-03 17:25:45 +0100

    refactor: remove unused 'cardsDispatch' in 'CardsList'

--- src/components/main/CardsList.tsx
+++ src/components/main/CardsList.tsx
@@ -1,18 +1,16 @@
 //  TODO: Card should be imported from ../cards/Card (finish implementing later).
-import { Action } from "App";
-import CardForm, { CardInterface } from "components/cardform/CardForm";
+import CardForm  from "components/cardform/CardForm";
 import Subheader from "components/Subheader";
 import { useState } from "react";
 import Card from "../cards/Card";
 import Maincard from "./Maincard";
-import { useCards, useUpdateCards } from "contexts/CardsContext";
+import { useCards } from "contexts/CardsContext";
 
 const CardsList: React.FC = () => {
   // TODO: the container should have no visible style (except probably for margins, etc. ) to make the app style and layout SIMPLER
   //       White background (for white theme) all the way to the left & right edges of the viewport.
 
   const cards = useCards()
-  const cardsDispatch = useUpdateCards();
 
   const [cardFormOpen, setCardFormOpen] = useState(false);
   const [cardId, setCardId] = useState<string>(""); // this cardId is going to be used to edit card in cardForm
