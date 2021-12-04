8061e47432efb3799775325c657d6a7777eb2a2d
Hamza Bow <hamzabow77@gmail.com>
2021-12-04 11:06:56 +0100

    refactor: remove unnecessary code inside Main.tsx

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,9 +1,7 @@
-import React, { useEffect } from "react";
+import React from "react";
 import CardsList from "./CardsList";
 import { Redirect } from "react-router";
 import { useAuth } from "../../contexts/AuthContext";
-import { CardsType } from "App"
-import { useParams } from "react-router-dom";
 
 interface Props {
   setCardsType: Function;
@@ -12,36 +10,7 @@ interface Props {
 
 const Main : React.FC<Props> = ({ setCardsType, setCollectionId }) => {
 
-  interface RouteParams {
-    id: string;
-  }
-  const params = useParams<RouteParams>();
 
-
-  useEffect(() => {
-
-    if (window.location.pathname.startsWith("/collection/")) {
-      if (setCollectionId !== undefined) {
-        setCollectionId(params.id);
-      } else {
-        throw new Error("setCollectionId function is undefined");
-      }
-      
-      if (setCardsType !== undefined) {
-        setCardsType(CardsType.CollectionCards);
-      } else {
-        throw new Error("setCardsType function is undefined");
-      }
-    } else if (window.location.pathname.startsWith("/user_cards")) {
-        setCardsType(CardsType.UserCards);
-    } else {
-      if (setCardsType !== undefined) {
-      setCardsType(CardsType.AllCards)
-      } else {
-        throw new Error("setCardsType function is undefined");
-      }
-    }
-  })
 
   const { currentUser } = useAuth() || { currentUser: undefined }
   return (
