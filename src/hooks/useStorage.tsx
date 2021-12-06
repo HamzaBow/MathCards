597cc4e01a76d926a34f573d1a0c4def9f1939c6
Hamza Bow <hamzabow77@gmail.com>
2021-12-06 23:03:55 +0100

    refactor: useStorage code to allow for exporting useSessionStorage hook

--- src/hooks/useStorage.tsx
+++ src/hooks/useStorage.tsx
@@ -1,20 +1,28 @@
 import { useEffect, useState } from "react";
 
-function getSavedValue(key: string, initialValue: string | Function): string {
-  const savedValue = JSON.parse(localStorage.getItem(key) as string);
+function getSavedValue(key: string, initialValue: string | Function, storage: Storage): string {
+  const savedValue = JSON.parse(storage.getItem(key) as string);
   if (savedValue) return savedValue;
 
   if (initialValue instanceof Function) return initialValue();
   return initialValue;
 }
 
-export default function useLocalStorage(key: string, initialValue: string | Function) {
+export function useLocalStorage(key: string, initialValue: string | Function) {
+  return useStorage(key, initialValue, window.localStorage)
+}
+
+export function useSessionStorage(key: string, initialValue: string | Function) {
+  return useStorage(key, initialValue, window.sessionStorage)
+}
+
+function useStorage(key: string, initialValue: string | Function, storage: Storage) {
   const [value, setValue] = useState(() => {
-    return getSavedValue(key, initialValue);
+    return getSavedValue(key, initialValue, storage);
   });
 
   useEffect(() => {
-    localStorage.setItem(key, JSON.stringify(value));
+    storage.setItem(key, JSON.stringify(value));
   }, [value]);
 
   return [value, setValue];
