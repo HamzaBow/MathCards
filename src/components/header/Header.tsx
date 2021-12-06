57d730cd43d7491409ddb243c60db6de1d4663fb
Hamza Bow <hamzabow77@gmail.com>
2021-12-06 22:04:43 +0100

    feat: move searchbar from 'Header.tsx' into a separate file

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,16 +1,13 @@
 import React, { useState, useRef } from "react";
-import { alpha } from "@mui/material/styles";
 import makeStyles from "@mui/styles/makeStyles";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
 import Check from "@mui/icons-material/Check";
-import InputBase from "@mui/material/InputBase";
 import Badge from "@mui/material/Badge";
 import MenuItem from "@mui/material/MenuItem";
 import Menu from "@mui/material/Menu";
 import MenuIcon from "@mui/icons-material/Menu";
-import SearchIcon from "@mui/icons-material/Search";
 import AccountCircle from "@mui/icons-material/AccountCircle";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
 import NotificationsIcon from "@mui/icons-material/Notifications";
@@ -36,6 +33,7 @@ import { UserActions, useUserUpdate } from "contexts/UserContext";
 import ArrowBack from "@mui/icons-material/ArrowBack";
 import CardForm from "components/cardform/CardForm";
 import { CardsActions, useUpdateCards } from "contexts/CardsContext";
+import SearchBar from "./SearchBar";
 
 const useStyles = makeStyles((theme) => ({
   grow: {
@@ -48,63 +46,6 @@ const useStyles = makeStyles((theme) => ({
     display: "none",
     [theme.breakpoints.up("sm")]: {
       display: "block",
-    },
-  },
-  search: {
-    position: "relative",
-    borderRadius: theme.shape.borderRadius,
-    borderWidth: "1px",
-    backgroundColor: theme.palette.background.default,
-    borderColor:
-      theme.palette.mode === "dark"
-        ? alpha(theme.palette.common.white, 0.25)
-        : alpha(theme.palette.common.black, 0.25),
-    borderStyle: "solid",
-    "&:hover": {
-      borderWidth: "2px",
-      borderColor:
-        theme.palette.mode === "dark"
-          ? alpha(theme.palette.common.white, 0.5)
-          : alpha(theme.palette.common.black, 0.45),
-    },
-    "&:focus-within": {
-      borderWidth: "3px",
-      borderColor:
-        theme.palette.mode === "dark"
-          ? alpha(theme.palette.common.white, 0.5)
-          : alpha(theme.palette.common.black, 0.45),
-    },
-    marginRight: theme.spacing(2),
-    marginLeft: 0,
-    width: "100%",
-    [theme.breakpoints.up("md")]: {
-      marginLeft: theme.spacing(3),
-      width: "auto",
-      position: "absolute",
-      left: "50%",
-      transform: "translateX(-50%)",
-    },
-  },
-  searchIcon: {
-    padding: theme.spacing(0, 1),
-    height: "100%",
-    position: "absolute",
-    pointerEvents: "none",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    zIndex: 1,
-  },
-  inputRoot: {
-    color: "inherit",
-  },
-  inputInput: {
-    padding: theme.spacing(1, 1, 1, 0),
-    paddingLeft: `calc(1em + ${theme.spacing(3)})`,
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("md")]: {
-      width: "40ch",
     },
   },
   sectionDesktop: {
@@ -373,16 +314,6 @@ const Header: React.FC = () => {
     </Menu>
   );
 
-  function handleSearbarFocus() {
-    if (window.innerWidth < 600 && logoRef?.current?.style) {
-      logoRef.current.style.width = "0px";
-    }
-  }
-  function handleSearbarBlur() {
-    if (logoRef?.current?.style) {
-      logoRef.current.style.width = "100%";
-    }
-  }
   return (
     <div className={classes.grow}>
       <AppBar position="static" color="inherit">
@@ -415,21 +346,7 @@ const Header: React.FC = () => {
               </IconButton>
             </div>
           </Tooltip>
-          <div className={classes.search}>
-            <div className={classes.searchIcon}>
-              <SearchIcon />
-            </div>
-            <InputBase
-              placeholder="Search…"
-              classes={{
-                root: classes.inputRoot,
-                input: classes.inputInput,
-              }}
-              inputProps={{ "aria-label": "search" }}
-              onFocus={handleSearbarFocus}
-              onBlur={handleSearbarBlur}
-            />
-          </div>
+          <SearchBar logoRef={logoRef} />
           <div className={classes.grow} />
           <div className={classes.sectionDesktop}>
             <Tooltip title="Create a new card">
---
+++ src/components/header/SearchBar.tsx
@@ -0,0 +1,107 @@
+import React from "react";
+import InputBase from "@mui/material/InputBase";
+import SearchIcon from "@mui/icons-material/Search";
+import { alpha } from "@mui/material/styles";
+import makeStyles from "@mui/styles/makeStyles";
+import { useHistory } from "react-router";
+
+const useStyles = makeStyles((theme) => ({
+  search: {
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    borderWidth: "1px",
+    backgroundColor: theme.palette.background.default,
+    borderColor:
+      theme.palette.mode === "dark"
+        ? alpha(theme.palette.common.white, 0.25)
+        : alpha(theme.palette.common.black, 0.25),
+    borderStyle: "solid",
+    "&:hover": {
+      borderWidth: "2px",
+      borderColor:
+        theme.palette.mode === "dark"
+          ? alpha(theme.palette.common.white, 0.5)
+          : alpha(theme.palette.common.black, 0.45),
+    },
+    "&:focus-within": {
+      borderWidth: "3px",
+      borderColor:
+        theme.palette.mode === "dark"
+          ? alpha(theme.palette.common.white, 0.5)
+          : alpha(theme.palette.common.black, 0.45),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      marginLeft: theme.spacing(3),
+      width: "auto",
+      position: "absolute",
+      left: "50%",
+      transform: "translateX(-50%)",
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 1),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    zIndex: 1,
+  },
+  inputRoot: {
+    color: "inherit",
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(3)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "40ch",
+    },
+  },
+}))
+
+interface Props {
+  logoRef: React.RefObject<HTMLDivElement>;
+}
+
+const SearchBar: React.FC<Props> = ({ logoRef }) => {
+  const classes = useStyles();
+  const history = useHistory();
+  function handleSearbarFocus() {
+    if (window.innerWidth < 600 && logoRef?.current?.style) {
+      logoRef.current.style.width = "0px";
+    }
+  }
+  function handleSearbarBlur() {
+    if (logoRef?.current?.style) {
+      logoRef.current.style.width = "100%";
+    }
+  }
+  return (
+    <div className={classes.search}>
+      <div className={classes.searchIcon}>
+        <SearchIcon />
+      </div>
+      <InputBase
+        placeholder="Search…"
+        classes={{
+          root: classes.inputRoot,
+          input: classes.inputInput,
+        }}
+        inputProps={{ "aria-label": "search" }}
+        onFocus={handleSearbarFocus}
+        onBlur={handleSearbarBlur}
+        onChange={() => {
+          history.push("/search?q=query");
+        }}
+      />
+    </div>
+  );
+}
+
+export default SearchBar
