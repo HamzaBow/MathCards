09e3002ff9a72da7351c0e8469d8e7c09c09ccde
Hamza Bow <hamzabow77@gmail.com>
2021-12-08 13:47:52 +0100

    refactor: move '#opened-card' to a new file 'MaincardCore.tsx'

--- src/components/main/Maincard.tsx
+++ src/components/main/Maincard.tsx
@@ -1,9 +1,8 @@
-import { addStyles, StaticMathField } from "react-mathquill";
-import { useState, useEffect, useRef } from "react";
-import { HiLightBulb } from "react-icons/hi";
-import { FieldType } from '../cardform/CardForm'
-import { Backdrop, ClickAwayListener, Fade, Paper, Popper } from "@mui/material";
-import { useCards } from "contexts/CardsContext";
+import { addStyles } from "react-mathquill";
+import { useState } from "react";
+import { Backdrop, Fade, Popper } from "@mui/material";
+import MaincardCore from "./MaincardCore";
+
 addStyles();
 
 interface Props {
@@ -17,58 +16,8 @@ const Maincard: React.FC<Props> = ({
   mainCardOpen,
   setMainCardOpen,
 }) => {
-  const cards = useCards();
 
-  const chosenCard = cards.find((card) => card._id === cardId);
 
-  const [frontDisplayed, setFrontDisplayed] = useState(true);
-  const divToRotate = useRef<HTMLDivElement>(null);
-  const frontRef = useRef<HTMLDivElement>(null);
-  const backRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    // Make card's front and back faces the same size (width and height)
-    if (frontRef?.current && backRef?.current) {
-      const frontWidth = window
-        .getComputedStyle(frontRef?.current)
-        .getPropertyValue("width");
-      const frontHeight = window
-        .getComputedStyle(frontRef?.current)
-        .getPropertyValue("height");
-      const backWidth = window
-        .getComputedStyle(backRef?.current)
-        .getPropertyValue("width");
-      const backHeight = window
-        .getComputedStyle(backRef?.current)
-        .getPropertyValue("height");
-
-      const maxWidth = Math.max(
-        parseFloat(frontWidth.slice(0, -2)),
-        parseFloat(backWidth.slice(0, -2))
-      );
-      const maxHeight = Math.max(
-        parseFloat(frontHeight.slice(0, -2)),
-        parseFloat(backHeight.slice(0, -2))
-      );
-
-      frontRef.current.style.width = maxWidth + "px";
-      backRef.current.style.width = maxWidth + "px";
-      frontRef.current.style.height = maxHeight + "px";
-      backRef.current.style.height = maxHeight + "px";
-    }
-  }, [cardId, mainCardOpen]);
-
-  useEffect(() => {
-    if (divToRotate?.current) {
-      if (frontDisplayed) {
-        divToRotate.current.style.transform = "rotateY(0deg)";
-      } else {
-        divToRotate.current.style.transform = "rotateY(180deg)";
-      }
-    } else {
-      console.error("cant rotate card, check divToRotate");
-    }
-  }, [frontDisplayed, cardId]);
 
   // TODO: useEffect( ... , []) which is on mount, press <space> flips the card, addEventListener
 
@@ -76,14 +25,9 @@ const Maincard: React.FC<Props> = ({
   //   visibility: mainCardActive ? "visible" : "hidden",
   //   opacity: mainCardActive ? "1" : "0",
   // };
-  const cardIconStyle: any = {
-    color: "yellow",
-    width: "3rem",
-    height: "3rem",
-    position: "absolute",
-    top: "-3.5rem",
-  };
-  function handleClose() {
+  const [frontDisplayed, setFrontDisplayed] = useState(true);
+  function handleClose(e: MouseEvent | TouchEvent | KeyboardEvent) {
+    console.log('set mainCardOpen to false')
     setMainCardOpen(false)
     setTimeout(() => {
       setFrontDisplayed(true)
@@ -101,72 +45,13 @@ const Maincard: React.FC<Props> = ({
       {({ TransitionProps }) => (
         <Fade {...TransitionProps}>
           <Backdrop open={true}>
-            <ClickAwayListener onClickAway={handleClose}>
-              <div
-                id="opened-card"
-                className="container-item"
-                // style={mainCardStyle}
-                onClick={() => {
-                  // setFrontIsShown((shown) => !shown);
-                  setFrontDisplayed((displayed) => !displayed);
-                }}
-              >
-                <div ref={divToRotate} className="card">
-                  <Paper ref={frontRef} className="front">
-                    {chosenCard?.front.map((field, key) => {
-                      if (field.type === FieldType.Math) {
-                        return (
-                          <StaticMathField
-                            key={key}
-                            style={{ fontSize: "2rem" }}
-                          >
-                            {field.latex}
-                          </StaticMathField>
-                        );
-                      }
-                      if (field.type === FieldType.Text) {
-                        return (
-                          <div
-                            key={key}
-                            dangerouslySetInnerHTML={{
-                              __html: field.htmlContent || "",
-                            }}
-                          ></div>
-                        );
-                      }
-                      return <></>;
-                    })}
-                  </Paper>
-
-                  <Paper ref={backRef} className="back">
-                    <HiLightBulb style={cardIconStyle} />
-                    {chosenCard?.back.map((field, key) => {
-                      if (field.type === FieldType.Math) {
-                        return (
-                          <StaticMathField
-                            key={key}
-                            style={{ fontSize: "2rem" }}
-                          >
-                            {field.latex}
-                          </StaticMathField>
-                        );
-                      }
-                      if (field.type === FieldType.Text) {
-                        return (
-                          <div
-                            key={key}
-                            dangerouslySetInnerHTML={{
-                              __html: field.htmlContent || "",
-                            }}
-                          ></div>
-                        );
-                      }
-                      return <></>;
-                    })}
-                  </Paper>
-                </div>
-              </div>
-            </ClickAwayListener>
+              <MaincardCore
+                cardId={cardId}
+                mainCardOpen={mainCardOpen}
+                frontDisplayed={frontDisplayed}
+                setFrontDisplayed={setFrontDisplayed}
+                handleClose={handleClose}
+              />
           </Backdrop>
         </Fade>
       )}
---
+++ src/components/main/MaincardCore.tsx
@@ -0,0 +1,147 @@
+
+import { HiLightBulb } from "react-icons/hi";
+import { FieldType } from '../cardform/CardForm'
+import { StaticMathField } from "react-mathquill";
+import { useCards } from "contexts/CardsContext";
+
+import { ClickAwayListener, Paper} from "@mui/material";
+import { useEffect, useRef } from "react";
+
+interface Props {
+  cardId: string;
+  mainCardOpen: boolean;
+  frontDisplayed: boolean;
+  setFrontDisplayed:  React.Dispatch<React.SetStateAction<boolean>>;
+  handleClose: (e: MouseEvent | TouchEvent | KeyboardEvent) => void;
+}
+const MaincardCore: React.FC<Props> = ({
+  cardId,
+  mainCardOpen,
+  frontDisplayed,
+  setFrontDisplayed,
+  handleClose
+}) => {
+  const cardIconStyle: any = {
+    color: "yellow",
+    width: "3rem",
+    height: "3rem",
+    position: "absolute",
+    top: "-3.5rem",
+  };
+
+  const divToRotate = useRef<HTMLDivElement>(null);
+  const frontRef = useRef<HTMLDivElement>(null);
+  const backRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    // Make card's front and back faces the same size (width and height)
+    if (frontRef?.current && backRef?.current) {
+      const frontWidth = window
+        .getComputedStyle(frontRef?.current)
+        .getPropertyValue("width");
+      const frontHeight = window
+        .getComputedStyle(frontRef?.current)
+        .getPropertyValue("height");
+      const backWidth = window
+        .getComputedStyle(backRef?.current)
+        .getPropertyValue("width");
+      const backHeight = window
+        .getComputedStyle(backRef?.current)
+        .getPropertyValue("height");
+
+      const maxWidth = Math.max(
+        parseFloat(frontWidth.slice(0, -2)),
+        parseFloat(backWidth.slice(0, -2))
+      );
+      const maxHeight = Math.max(
+        parseFloat(frontHeight.slice(0, -2)),
+        parseFloat(backHeight.slice(0, -2))
+      );
+
+      frontRef.current.style.width = maxWidth + "px";
+      backRef.current.style.width = maxWidth + "px";
+      frontRef.current.style.height = maxHeight + "px";
+      backRef.current.style.height = maxHeight + "px";
+    }
+  }, [cardId, mainCardOpen]);
+
+  useEffect(() => {
+    if (divToRotate?.current) {
+      if (frontDisplayed) {
+        divToRotate.current.style.transform = "rotateY(0deg)";
+      } else {
+        divToRotate.current.style.transform = "rotateY(180deg)";
+      }
+    } else {
+      console.error("cant rotate card, check divToRotate");
+    }
+  }, [frontDisplayed, cardId]);
+  const cards = useCards();
+
+  const chosenCard = cards.find((card) => card._id === cardId);
+
+  return (
+    <ClickAwayListener onClickAway={(e) => {handleClose(e)}}>
+      <div
+        id="opened-card"
+        className="container-item"
+        // style={mainCardStyle}
+        onClick={() => {
+          // setFrontIsShown((shown) => !shown);
+          setFrontDisplayed((displayed) => !displayed);
+        }}
+      >
+        <div ref={divToRotate} className="card">
+          <Paper ref={frontRef} className="front">
+            {chosenCard?.front.map((field, key) => {
+              if (field.type === FieldType.Math) {
+                return (
+                  <StaticMathField key={key} style={{ fontSize: "2rem" }}>
+                    {field.latex}
+                  </StaticMathField>
+                );
+              }
+              if (field.type === FieldType.Text) {
+                return (
+                  <div
+                    key={key}
+                    dangerouslySetInnerHTML={{
+                      __html: field.htmlContent || "",
+                    }}
+                  ></div>
+                );
+              }
+              return <></>;
+            })}
+          </Paper>
+
+          <Paper ref={backRef} className="back">
+            <HiLightBulb style={cardIconStyle} />
+            {chosenCard?.back.map((field, key) => {
+              if (field.type === FieldType.Math) {
+                return (
+                  <StaticMathField key={key} style={{ fontSize: "2rem" }}>
+                    {field.latex}
+                  </StaticMathField>
+                );
+              }
+              if (field.type === FieldType.Text) {
+                return (
+                  <div
+                    key={key}
+                    dangerouslySetInnerHTML={{
+                      __html: field.htmlContent || "",
+                    }}
+                  ></div>
+                );
+              }
+              return <></>;
+            })}
+          </Paper>
+        </div>
+      </div>
+    </ClickAwayListener>
+  );
+};
+
+export default MaincardCore;
