1fd063a255e2fab1d0cdc23a877cc8147274f428
Hamza Bow <hamzabow77@gmail.com>
2022-04-11 18:37:08 +0100

    feat: add style and content to EmailNotVerified page

--- src/components/authentication/EmailNotVerifiedPage.tsx
+++ src/components/authentication/EmailNotVerifiedPage.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import { useAuth } from "contexts/AuthContext";
+import React, { useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import Paper from "@mui/material/Paper";
+import { Divider, Typography } from "@mui/material";
+import Box from "@mui/system/Box";
+import Logo from "components/Logo";
 
 const EmailNotVerifiedPage = () => {
+  const { currentUser } = useAuth();
+  const history = useHistory();
+  useEffect(() => {
+    if (currentUser?.emailVerified) {
+      history.push("/");
+    }
+  }, [currentUser?.emailVerified]);
   return (
-    <h1>EmailNotVerifiedPage</h1>
-  )
-}
+    <Box sx={{ display: "grid", placeItems: "center", height: "70vh" }}>
+      <Paper elevation={3} sx={{ p: 4, display: "grid", placeItems: "center", mx: 2 }}>
+        <Logo />
+        <Divider sx={{ width: 1, my: 3 }} />
+        <Typography
+          textAlign="center"
+          variant="body1"
+          component="h2"
+          sx={{ my: 1 }}
+        >
+          A verification link has been sent to your email account.
+        </Typography>
+        <Typography textAlign="center" variant="body1" component="h2">
+          Please, check out your email and click on the verification link so that you can
+          start using the app.
+        </Typography>
+      </Paper>
+    </Box>
+  );
+};
 
-export default EmailNotVerifiedPage
+export default EmailNotVerifiedPage;
