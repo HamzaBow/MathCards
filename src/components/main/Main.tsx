64f59fb6065744e71fd09187381f2e586f8795e2
Hamza Bow <hamzabow77@gmail.com>
2022-04-25 02:55:42 +0100

    fix: don't check if user authenticated in Main component

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,19 +1,19 @@
 import React from "react";
 import CardsList from "./CardsList";
-import { Redirect } from "react-router";
-import { useAuth } from "../../contexts/AuthContext";
+// import { Redirect } from "react-router";
+// import { useAuth } from "../../contexts/AuthContext";
 
 const Main : React.FC = () => {
-  const { currentUser } = useAuth() || { currentUser: undefined }
+//   const { currentUser } = useAuth() || { currentUser: undefined }
   return (
     <main>
-      {currentUser ?
+      {/* {currentUser ? */}
         <>
           <CardsList />
         </>
-        :
+        {/* :
         <Redirect to='/login' />
-      }
+      } */}
     </main>
   );
 };
